dc2608926e767c520eebce366aa1cc52db366f96
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-06-16 20:03:22 +0200

    Add EntitiesLoggerPlugin to handle entity logging

---
+++ src/EntitiesLoggerPlugin.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Cake\EntitiesLogger;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * A plugin class responsible for logging entities during application execution.
+ */
+class EntitiesLoggerPlugin extends BasePlugin
+{
+}
