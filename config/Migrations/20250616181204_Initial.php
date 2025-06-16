99c4dd284b0b5acc9f7faa4f0142bd64375335c6
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-06-16 20:12:24 +0200

    Add initial migration for entities_logs table setup

---
+++ config/Migrations/20250616181204_Initial.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\BaseMigration;
+
+class Initial extends BaseMigration
+{
+    /**
+     * Up Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-up-method
+     * @return void
+     */
+    public function up(): void
+    {
+        $this->table('entities_logs')
+            ->addColumn('entity_class', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('entity_id', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+                'signed' => true,
+            ])
+            ->addColumn('user_id', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+                'signed' => true,
+            ])
+            ->addColumn('type', 'string', [
+                'default' => null,
+                'limit' => 100,
+                'null' => false,
+            ])
+            ->addColumn('datetime', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->create();
+    }
+
+    /**
+     * Down Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-down-method
+     * @return void
+     */
+    public function down(): void
+    {
+        $this->table('entities_logs')->drop()->save();
+    }
+}
