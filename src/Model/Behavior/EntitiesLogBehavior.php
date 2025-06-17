fa2650d53a316e8f38e6724b395137a02d63520c
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-06-17 10:00:18 +0200

    Refactor EntitiesLogBehavior to add afterDelete handling and simplify log entity creation

--- src/Model/Behavior/EntitiesLogBehavior.php
+++ src/Model/Behavior/EntitiesLogBehavior.php
@@ -5,11 +5,14 @@ namespace Cake\EntitiesLogger\Model\Behavior;
 
 use Authentication\IdentityInterface;
 use Cake\Datasource\EntityInterface;
+use Cake\EntitiesLogger\Model\Entity\EntitiesLog;
 use Cake\EntitiesLogger\Model\Enum\EntitiesLogType;
+use Cake\EntitiesLogger\Model\Table\EntitiesLogsTable;
 use Cake\Event\EventInterface;
 use Cake\I18n\DateTime;
 use Cake\ORM\Behavior;
 use Cake\ORM\Locator\LocatorAwareTrait;
+use Cake\ORM\Table;
 use Cake\Routing\Router;
 
 /**
@@ -23,6 +26,18 @@ use Cake\Routing\Router;
 class EntitiesLogBehavior extends Behavior
 {
     use LocatorAwareTrait;
+
+    protected EntitiesLogsTable $EntitiesLogsTable;
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct(Table $table, array $config = [])
+    {
+        parent::__construct($table, $config);
+
+        $this->EntitiesLogsTable = $this->fetchTable('Cake/EntitiesLogger.EntitiesLogs');
+    }
 
     /**
      * Retrieves the identity of the currently authenticated user.
@@ -39,6 +54,24 @@ class EntitiesLogBehavior extends Behavior
         return $Identity;
     }
 
+    /**
+     * Constructs a new EntitiesLog entity based on the provided parameters.
+     *
+     * @param \Cake\Datasource\EntityInterface $entity The entity object related to the log entry.
+     * @param \Cake\EntitiesLogger\Model\Enum\EntitiesLogType $entitiesLogType The type of log to be created.
+     * @return \Cake\EntitiesLogger\Model\Entity\EntitiesLog The newly created EntitiesLog entity.
+     */
+    protected function buildEntity(EntityInterface $entity, EntitiesLogType $entitiesLogType): EntitiesLog
+    {
+        return $this->EntitiesLogsTable->newEntity([
+            'entity_class' => $entity::class,
+            'entity_id' => $entity->id,
+            'user_id' => $this->getIdentity()->id,
+            'type' => $entitiesLogType,
+            'datetime' => new DateTime(),
+        ]);
+    }
+
    /**
      * Handles actions to be performed after an entity is saved.
      *
@@ -48,17 +81,23 @@ class EntitiesLogBehavior extends Behavior
      */
     public function afterSave(EventInterface $event, EntityInterface $entity): void
     {
-        /** @var \Cake\EntitiesLogger\Model\Table\EntitiesLogsTable $EntitiesLogsTable */
-        $EntitiesLogsTable = $this->fetchTable('Cake/EntitiesLogger.EntitiesLogs');
+        $entitiesLogType = $entity->isNew() ? EntitiesLogType::Created : EntitiesLogType::Updated;
+        $EntitiesLog = $this->buildEntity(entity: $entity, entitiesLogType: $entitiesLogType);
 
-        $EntitiesLog = $EntitiesLogsTable->newEntity([
-            'entity_class' => $entity::class,
-            'entity_id' => $entity->id,
-            'user_id' => $this->getIdentity()->id,
-            'type' => $entity->isNew() ? EntitiesLogType::Created : EntitiesLogType::Updated,
-            'datetime' => new DateTime(),
-        ]);
+        $this->EntitiesLogsTable->saveOrFail($EntitiesLog);
+    }
 
-        $EntitiesLogsTable->saveOrFail($EntitiesLog);
+    /**
+     * Handles actions to be performed after an entity is deleted.
+     *
+     * @param \Cake\Event\EventInterface $event The event that triggered the method.
+     * @param \Cake\Datasource\EntityInterface $entity The entity object that was deleted.
+     * @return void
+     */
+    public function afterDelete(EventInterface $event, EntityInterface $entity)
+    {
+        $EntitiesLog = $this->buildEntity(entity: $entity, entitiesLogType: EntitiesLogType::Deleted);
+
+        $this->EntitiesLogsTable->saveOrFail($EntitiesLog);
     }
 }
