49d4a66d6807320497ae127e85e2fce39c1cdecc
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-06-18 12:10:06 +0200

    Add `Article` entity and unit test for `buildEntity` in `EntitiesLogBehavior`

--- tests/TestCase/Model/Behavior/EntitiesLogBehaviorTest.php
+++ tests/TestCase/Model/Behavior/EntitiesLogBehaviorTest.php
@@ -3,12 +3,14 @@ declare(strict_types=1);
 
 namespace Cake\EntitiesLogger\Test\TestCase\Model\Behavior;
 
+use App\Model\Entity\Article;
 use Cake\Datasource\EntityInterface;
 use Cake\Datasource\Exception\MissingPropertyException;
 use Cake\EntitiesLogger\Model\Behavior\EntitiesLogBehavior;
 use Cake\EntitiesLogger\Model\Entity\EntitiesLog;
 use Cake\EntitiesLogger\Model\Enum\EntitiesLogType;
 use Cake\Http\ServerRequest;
+use Cake\I18n\DateTime;
 use Cake\ORM\Entity;
 use Cake\ORM\Table;
 use Cake\Routing\Router;
@@ -89,6 +91,40 @@ class EntitiesLogBehaviorTest extends TestCase
         $this->expectException(MissingPropertyException::class);
         $this->expectExceptionMessage('`' . Entity::class . '::$id` is null, expected non-null value.');
         $Behavior->getIdentityId();
+    }
+
+    #[Test]
+    public function testBuildEntity(): void
+    {
+        $expectedKeys = [
+            'entity_class',
+            'entity_id',
+            'user_id',
+            'type',
+            'datetime',
+        ];
+
+        $Behavior = new class (new Table()) extends EntitiesLogBehavior {
+            protected function getIdentityId(): int
+            {
+                return 2;
+            }
+
+            public function buildEntity(EntityInterface $entity, EntitiesLogType $entitiesLogType): EntitiesLog
+            {
+                return parent::buildEntity($entity, $entitiesLogType);
+            }
+        };
+
+        $result = $Behavior->buildEntity(new Article(['id' => 3]), EntitiesLogType::Created);
+
+        $this->assertSame($expectedKeys, array_keys($result->toArray()));
+        $this->assertSame(Article::class, $result->entity_class);
+        $this->assertSame(3, $result->entity_id);
+        $this->assertSame(2, $result->user_id);
+        $this->assertSame(EntitiesLogType::Created, $result->type);
+        $this->assertInstanceOf(DateTime::class, $result->datetime);
+        $this->assertLessThanOrEqual(1, $result->datetime->diffInSeconds());
     }
 
     #[Test]
