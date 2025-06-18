3be5daf9f18660cf42f8766d9a3e4bdb843b9395
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-06-18 09:25:36 +0200

    Update test bootstrap to include database migrations setup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,21 @@
 <?php
 declare(strict_types=1);
+
+use Cake\Datasource\ConnectionManager;
+use Migrations\TestSuite\Migrator;
+
+define('ROOT', dirname(__DIR__) . DS);
+const CORE_PATH = ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS;
+define('TMP', sys_get_temp_dir() . DS . 'cakephp-entities-logger' . DS);
+
+// phpcs:disable
+@mkdir(TMP, 0777, true);
+// phpcs:enable
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+require_once CORE_PATH . 'config' . DS . 'bootstrap.php';
+
+ConnectionManager::setConfig('test', ['url' => 'sqlite:///' . TMP . 'test.sq3']);
+
+$migrator = new Migrator();
+$migrator->run();
