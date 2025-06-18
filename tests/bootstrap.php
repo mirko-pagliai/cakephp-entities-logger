ede6f2fc142f0e87c3bac82d2a16484971deec15
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-06-18 12:00:16 +0200

    Enable debug mode in test bootstrap configuration.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 
+use Cake\Core\Configure;
 use Cake\Datasource\ConnectionManager;
 use Migrations\TestSuite\Migrator;
 
@@ -14,6 +15,8 @@ define('TMP', sys_get_temp_dir() . DS . 'cakephp-entities-logger' . DS);
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 require_once CORE_PATH . 'config' . DS . 'bootstrap.php';
+
+Configure::write('debug', true);
 
 ConnectionManager::setConfig('test', ['url' => 'sqlite:///' . TMP . 'test.sq3']);
 
